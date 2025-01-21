562beefa41b0067b3d980ac1a1fb9e66265c98ca
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-21 16:36:58 -0600

    Initial Commit

---
+++ Poke3DTests/Poke3DTests.swift
@@ -0,0 +1,17 @@
+//
+//  Poke3DTests.swift
+//  Poke3DTests
+//
+//  Created by Isaac Urbina on 1/21/25.
+//
+
+import Testing
+@testable import Poke3D
+
+struct Poke3DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
